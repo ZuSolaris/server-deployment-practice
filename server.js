3567df7e4cf8aa48d34bf8b646ee16e2f95ac79e
Zoe Gonzalez <zusolaris.personal@gmail.com>
2022-11-28 22:40:08 +0000

    error handlers added

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 3002;
-
+const logger = require('./middleware/logger');
+const notFound = require('./handlers/404');
+const errorHandler = require('./handlers/500');
 //singleton instance of express
 const app = express();
 
@@ -11,10 +13,18 @@ const app = express();
 
 app.use(cors());
 
+app.use(logger);
 
-app.get('/', (req, res, next) => {
+app.get('/', logger, (req, res, next) => {
 res.status(200).send('Hello World');
 });
+
+app.get('/bad', (req, res, next) => {
+  next('We have a problem');
+});
+
+app.use('*', notFound);
+app.use(errorHandler);
 
 function start() {
 app.listen(PORT, () => console.log('Up on PORT', PORT));
